021762fbcad46738545c01da11170e04de7319fd
vaibhav sharma <vaibhav.sharma@outlook.com>
2025-02-25 23:20:45 +0530

    click commands added

--- client.py
+++ client.py
@@ -1,5 +1,14 @@
 import uvicorn
+import click
 from multiprocessing import cpu_count
 
+@click.command
+@click.option('--cpu',default=cpu_count()*2+1)
+@click.option('--host',default='0.0.0.0')
+@click.option('--port',default=8005)
+@click.option('--dev',default=False)
+def uvicorn_app(host,port,cpu,dev):
+    uvicorn.run('server_app.list_server:app', host=host, port=port,workers=cpu,reload=dev)
+
 if (__name__ == '__main__'):
-    uvicorn.run('server_app.list_server:app', host='0.0.0.0', port=8005,workers=cpu_count()*2+1)
+    uvicorn_app()
